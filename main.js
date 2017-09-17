54250e39d003dbf81b98a82dc4ec26edfa8b2b76
Malte Bargholz <malte@screenri.de>
2017-09-17 13:46:46 +0200

    add simple parser code

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 "use strict;"
 
+let yaml = require('js-yaml');
+let fs = require('fs');
+
+try{
+    let doc = yaml.safeLoad(fs.readFileSync('./protocol.yaml'));
+    console.log(JSON.stringify(doc,null,'\t'));
+} catch(e){
+    console.error(e);
+}
 
