cabe2e9ffaf6967bc6b3b46e923b9f279ed82c55
Malte Bargholz <malte@screenri.de>
2018-05-02 19:41:50 +0200

    fix bugs in plugin tester

--- cmds/plugin_cmds/test.js
+++ cmds/plugin_cmds/test.js
@@ -1,6 +1,6 @@
 'use strict;'
 // imports
-import 'io' from 'socket.io-client'
+const io = require('socket.io-client')
 let socket
 
 module.exports.command = 'test <ids...>'
@@ -15,40 +15,58 @@ module.exports.handler = (argv) => {
     log.info(`got ${argv.ids.length} ids.`)
     argv.ids.forEach(id => log.verbose(`- ${id}`))
     
-    let finished = false
     let resultsPending = []
     const api = argv.development ? argv.developmentAPI : argv.productionAPI
-    socket = io(api)
+    socket = io(api, {
+        timeout: 2000
+    })
+
     socket.on('action error', (error) => {
-        log.error(`got error while executing an action:\n`)
-        log.error(error)
-        finished = true
+        if(error && error.error && error.actionId) {
+            log.error(`action ${error.actionId} (rid = ${error.resultId}) error: `, error.error)
+            if(error.resultId) {
+                resultsPending = resultsPending.filter(elem => elem.resultId !== error.resultId)
+                if(resultsPending.length === 0) {
+                    log.info('finished testing.')
+                    socket.close()
+                }
+            }
+        }
+        else {
+            log.error('an undefined action has thrown an undefined error') 
+            socket.close()
+        }
     })
     socket.on('result', (answer) => {
-        log.info(`got result:\n`, answer)
-        resultsPending = resultsPending.filter((elem) => elem != answer.id)
-        if(resultsPending.length === 0) finished = true
+        const obj = resultsPending.find((elem) => elem.resultId === answer.id);
+        log.warn(`action ${obj.actionId} (rid = ${obj.resultId}) result : `, answer.result)
+        resultsPending = resultsPending.filter((elem) => elem.resultId !== answer.id)
+        if(resultsPending.length === 0) {
+            log.info('finished testing.')
+            socket.close()
+        }
     })
     socket.on('error', (msg) => {
-        log.error(`got error from socket.io:\n`)
-        log.error(error)
-        finished = true
+        log.error(`socket.io error: `, msg)
     })
-    socket.on('connection', () => {
+
+    socket.on('connect_error', (error) => {
+        log.warn(`error while connecting to socket.io@${api}: `, error) 
+    })
+
+    socket.on('connect', () => {
         log.info(`connected to socket.io@${api}`)
         for(const id of argv.ids)  {
             socket.emit('action', id, (resultid) => {
-                resultsPending.push(resultid)
+                log.debug(`action ${id} -> result ${resultid}`)
+                resultsPending.push({ resultId: resultid, actionId: id })
             })
         }
     })
     socket.on('disconnect', () => {
         log.info(`disconnected from socket.io@${api}`)
-        finished = true;
     })
 
-    while(!finished) {
-        //do nothing 
-    }
+    socket.open()
 }
 
